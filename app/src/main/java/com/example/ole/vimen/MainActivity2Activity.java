45955463ef824fff0a31eb371a5d303f23532c17
Appday <atzetooon@googlemail.com>
2015-05-30 07:51:34 +0200

    added missing files

---
+++ app/src/main/java/com/example/ole/vimen/MainActivity2Activity.java
@@ -0,0 +1,56 @@
+package com.example.ole.vimen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+
+
+
+public class MainActivity2Activity extends Activity {
+
+    //private Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2);
+
+        //spinner=(Spinner) findViewById(R.id.spinner);
+    }
+
+    public void Main(View v){
+        Button mainButton = (Button) v;
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
